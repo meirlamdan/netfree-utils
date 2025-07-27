9fb1fec342795ab11af6daaf962e750ddddd18eb
meir lerner <meir3252@gmail.com>
2025-07-27 13:37:48 +0300

    add popup showing blocked URLs

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ A Chrome extension for netfree users.
 
 - check link if it's blocked by netfree or not, (when hover over link and press alt key or ctrl key)
 - replace blocked youtube iframes with a placeholder with the video thumbnail
-- more features in the future
+- Floating popup showing blocked URLs on the current tab (this feature is based on the great idea and implementation in [NetFree Helper](https://github.com/yossef7525/netfree-helper) by [yossef7525](https://github.com/yossef7525))
+- More features coming in the future
 
+## Installation
 
+1. Clone the repository
+2. Open chrome://extensions/ in your browser
+3. Enable developer mode
+4. Click on "Load unpacked" and select the cloned repository folder
--- background.js
+++ background.js
@@ -16,3 +16,36 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     return true;
   }
 });
+
+let blockedPerTab = {};
+chrome.webRequest.onCompleted.addListener(
+  function (details) {
+    if (details.statusCode === 418) {
+      const tabId = details.tabId;
+
+      if (!blockedPerTab[tabId]) {
+        blockedPerTab[tabId] = [];
+      }
+      
+      const existing = blockedPerTab[tabId].find(url => url === details.url);
+      if (existing) {
+        return;
+      }
+
+      blockedPerTab[tabId].push(
+        details.url
+      );
+
+      chrome.storage.local.set({ blockedPerTab });
+
+      chrome.action.setBadgeText({ text: blockedPerTab[tabId].length.toString(), tabId });
+      chrome.action.setBadgeBackgroundColor({ color: "#ff0000", tabId });
+    }
+  },
+  { urls: ["<all_urls>"] }
+);
+
+chrome.tabs.onRemoved.addListener(function (tabId) {
+  delete blockedPerTab[tabId];
+  chrome.storage.local.set({ blockedPerTab });
+});
